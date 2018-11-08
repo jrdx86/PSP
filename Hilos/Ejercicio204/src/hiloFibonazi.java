fdd330f5a5361017f2a9fff1649da97a4127c8af
jrdx86 <jrdx86@gmail.com>
2018-11-08 12:16:02 +0100

    hilos

--- Hilos/Ejercicio204/src/hiloFibonazi.java
+++ Hilos/Ejercicio204/src/hiloFibonazi.java
@@ -1,4 +1,13 @@
 
-public class hiloFibonazi {
-
+public class hiloFibonazi extends Thread{
+	public int numero=0;
+	
+	public hiloFibonazi(int numero) {
+		this.numero=numero;
+	}
+	public void run() {
+		for ()
+		
+		
+	}
 }
